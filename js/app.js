30923e1ef3b262a2bad0d2a7953971e8fc7f4122
Roman Gebrehiwot <rttgg1@gmail.com>
2019-07-08 13:13:24 -0700

    added sort

--- js/app.js
+++ js/app.js
@@ -1,31 +1,37 @@
 'use strict';
 
 const allImages = [];
+const allImages2 = [];
 let include = [];
 let imageArr = [];
 let flag = true;
+let doIt = false;
 let filePath;
 let fileType;
+
 const Images = function (image_url, title, description, keyword, horns) {
   this.image_url = image_url;
   this.title = title;
   this.description = description;
   this.keyword = keyword;
   this.horns = horns;
-  allImages.push(this);
+  if(filePath ==='./data/page-1.json' ){
+    allImages.push(this);
+  }
+  else if(filePath === './data/page-2.json' ){
+    console.log('hello');
+    allImages2.push(this);
+  }
+  console.log(allImages2);
 };
-$('button').on('click',function(){
 
-  $('#photo-template').replaceAll();
-  if(flag === false){
-    flag = true;
+$('button').click(function(){
+  if(doIt === false){
+    Images.getAllimagesFromFile2();
+    doIt = true;
   }
-  else if(flag === true){
-    flag = false;
-  }
-
-  Images.getAllimagesFromFile();
-  renderDropDown();
+  $( '.json1' ).toggle( 'slow', function() {
+  });
 });
 
 $('select').on('click', function () {
@@ -52,27 +58,28 @@ const renderDropDown = function () {
     $(selectEl).append($newOption);
   });
 };
+Images.prototype.renderWithJqAfter = function(){
 
-Images.prototype.renderWithJq = function () {
-
-  var source   = document.getElementById('entry-template').innerHTML;
-  var template = Handlebars.compile(source);
-  var context = {title: this.title, keyword: this.keyword, description: this.description, horns: this.horns, image: this.image_url};
+};
+Images.prototype.renderWithJq = function (){
+  if(flag === true){
+    var source   = document.getElementById('entry-template').innerHTML;
+    var template = Handlebars.compile(source);
+    var context = {title: this.title, keyword: this.keyword, description: this.description, horns: this.horns, image: this.image_url};
+  }
+  else if(flag === false){
+    source = document.getElementById('entry-template2').innerHTML;
+    template = Handlebars.compile(source);
+    context = {title2: this.title, keyword2: this.keyword, description2: this.description, horns2: this.horns, image2: this.image_url};
+  }
   var html = template(context);
 
   $('main').append(html);
 };
 
 Images.getAllimagesFromFile = function () {
-  console.log(flag);
-  if(flag === true){
-    filePath = './data/page-1.json';
-    fileType = 'json';
-  }
-  else if(flag === false){
-    filePath = './data/page-2.json';
-    fileType = 'json';
-  }
+  filePath = './data/page-1.json';
+  fileType = 'json';
   $.get(filePath, fileType).then(myImagesJSON => {
     myImagesJSON.forEach(image => {
       new Images(image.image_url, image.title, image.description, image.keyword, image.horns);
@@ -84,11 +91,36 @@ Images.getAllimagesFromFile = function () {
     $.each(imageArr, function (value, index) {
       if ($.inArray(index, include) === -1) include.push(index);
     });
-    allImages.forEach(image => {
+    if(allImages.length < 21){
+      allImages.forEach(image => {
+        image.renderWithJq();
+      });
+    }
+    renderDropDown();
+  });
+};
+Images.getAllimagesFromFile2 = function () {
+  filePath = './data/page-2.json';
+  fileType = 'json';
+  $.get(filePath, fileType).then(myImagesJSON => {
+    myImagesJSON.forEach(image => {
+      new Images(image.image_url, image.title, image.description, image.keyword, image.horns);
+    });
+    allImages.forEach((value, index) => {
+      imageArr.push(allImages[index].keyword);
+    });
+    include.push('default');
+    $.each(imageArr, function (value, index) {
+      if ($.inArray(index, include) === -1) include.push(index);
+    });
+    allImages2.forEach(image => {
       image.renderWithJq();
     });
     renderDropDown();
   });
+  $('.json2').hide();
 };
 Images.getAllimagesFromFile();
+
+
 
